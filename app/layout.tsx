d6abace81ee377972baef21e73e54ee8024cf710
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-07-19 01:53:52 -0700

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 import './globals.css'
 import type { Metadata } from 'next'
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { Inter } from 'next/font/google'
 import TopBar from './Components/TopBar'
 import Header from './Components/Header'
@@ -24,6 +24,27 @@ export default function RootLayout({
   const setLang = (data: boolean) => {
     setlanguagePopupStatus(!languagePopupStatus)
   }
+  function useOutsideAlerter(ref: any) {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event: any) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          // setlanguagePopupStatus(false);
+          console.log(event.target)
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
   return (
     <html lang="en">
       <body className={`${inter.className} w-full min-h-screen font-Poppins`}>
@@ -32,7 +53,7 @@ export default function RootLayout({
           <Header setLang={setLang} />
           <Language languagePopupStatus={languagePopupStatus} />
         </div>
-        <main>
+        <main ref={wrapperRef}>
           {children}
         </main>
         <div className='w-full'>
