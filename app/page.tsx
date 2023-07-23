8fc0363bbdd1865d262619acdfd3cbdd3ad9778a
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-07-23 06:53:16 -0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ const Mobile = (props:any) => {
               <div className='w-full'>
                 <h1 className='text-xl font-Kanit font-bold text-center'>Why Do I Need to Use a VPN ?</h1>
               </div>
-              <div className='py-5 px-5 w-full'>
-                <p className='text-sm font-medium w-full text-justify'>
+              <div className='py-5 px-5 w-full mb-5'>
+                <p className='text-sm font-normal w-full text-justify'>
                   A VPN (Virtual Private Network) is essential as it encrypts your internet traffic, ensuring your online activities and data remain private and secure. It becomes crucial on public Wi-Fi networks to protect sensitive information from potential threats.
 
                   VPN allows bypassing geo-restrictions, accessing content and services blocked in your region, such as streaming platforms and websites. By masking your IP address, it enables anonymous browsing, preventing online tracking.
@@ -46,7 +46,7 @@ const Mobile = (props:any) => {
                 <h1 className='text-xl mt-10 mb-5 font-Kanit font-bold text-center'>How Can VPN Protected us in Internet ?</h1>
               </div>
               <div className='px-5 h-full bg-white'>
-                <p className='text-sm font-medium w-full text-justify text-black'>
+                <p className='text-sm font-normal w-full text-justify text-black'>
                   A VPN (Virtual Private Network) protects us on the internet by encrypting our internet traffic, ensuring that our online activities and data remain confidential and safe from potential threats like hackers and surveillance.
 
                   When using public Wi-Fi networks, a VPN acts as a shield, preventing malicious entities from intercepting our sensitive information, reducing the risk of data breaches.
@@ -63,7 +63,6 @@ const Mobile = (props:any) => {
                 </p>
               </div>
             </div>
-
           </div>
         </div>
       </section>
