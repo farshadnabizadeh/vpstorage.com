c231e5ea46b07263fbb6cb48e20913b5e14d4fc5
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-07-25 01:48:34 -0700

    vercel

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const notFound = () => {
+    return (
+        <div id="notfound">
+            <div className="notfound">
+                <div className="notfound-404">
+                    <h1>4<span>0</span>4</h1>
+                </div>
+                <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+                <a className='' href="#"><span className='pt-2'>Home page</span></a>
+            </div>
+        </div>
+    )
+}
+
+export default notFound
