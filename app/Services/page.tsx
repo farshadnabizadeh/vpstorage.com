8923f26b7d5c89f15a217d1dac84760f399d50b9
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-07-27 01:21:36 -0700

    updated

--- app/Services/page.tsx
+++ app/Services/page.tsx
@@ -1,38 +1,63 @@
 import React, { FC } from 'react'
-
+import SuccesssSVG from '@/assets/svg/success.svg'
+import Image from 'next/image'
 const page: FC = () => {
   return (
     <div className='w-full min-h-screen bg-white flex justify-center'>
       <div className='container h-full'>
         <div className='w-full h-full grid grid-cols-3 gap-28 translate-y-[65px]'>
-          <div className='my-10 rounded-lg'>
+          <div className='my-10 rounded-lg border-[1px] border-[#555]'>
             <div className='rounded-t-lg w-full h-14 bg-[#212F3C] text-base text-[#fff] flex justify-center items-center tracking-[0.08rem]'>
               Plan 1
             </div>
-            <div className='w-full h-[150px] flex justify-center items-center border-[1px] border-[#555] rounded-b-lg'>
+            <div className='w-full h-[100px] flex justify-center items-center'>
               <div className='text-base font-semibold text-[#000] mx-1'>$</div>
               <div className='text-xl font-semibold text-[#000]'>200</div>
               <div className='text-base font-semibold text-[#000]'>.88</div>
+            </div>
+            <div className='w-full'>
+              <div className='flex items-center'>
+                  <div className='w-8 h-8 relative translate-x-[20px]'>
+                    <Image fill src={SuccesssSVG} alt="plans"/>
+                  </div>
+                  <div className='text-base text-[#555] font-semibold flex items-center translate-x-[50px]'>2 CPU cores</div>
+              </div>
             </div>
           </div>
-          <div className='my-10 rounded-lg'>
+          <div className='my-10 rounded-lg border-[1px] border-[#555]'>
             <div className='rounded-t-lg w-full h-14 bg-[#212F3C] text-base text-[#fff] flex justify-center items-center tracking-[0.08rem]'>
               Plan 1
             </div>
-            <div className='w-full h-[150px] flex justify-center items-center'>
+            <div className='w-full h-[100px] flex justify-center items-center'>
               <div className='text-base font-semibold text-[#000] mx-1'>$</div>
               <div className='text-xl font-semibold text-[#000]'>200</div>
               <div className='text-base font-semibold text-[#000]'>.88</div>
+            </div>
+            <div className='w-full'>
+              <div className='flex items-center'>
+                  <div className='w-8 h-8 relative translate-x-[20px]'>
+                    <Image fill src={SuccesssSVG} alt="plans"/>
+                  </div>
+                  <div></div>
+              </div>
             </div>
           </div>
-          <div className='my-10 rounded-lg'>
+          <div className='my-10 rounded-lg border-[1px] border-[#555]'>
             <div className='rounded-t-lg w-full h-14 bg-[#212F3C] text-base text-[#fff] flex justify-center items-center tracking-[0.08rem]'>
               Plan 1
             </div>
-            <div className='w-full h-[150px] flex justify-center items-center'>
+            <div className='w-full h-[100px] flex justify-center items-center'>
               <div className='text-base font-semibold text-[#000] mx-1'>$</div>
               <div className='text-xl font-semibold text-[#000]'>200</div>
               <div className='text-base font-semibold text-[#000]'>.88</div>
+            </div>
+            <div className='w-full'>
+              <div className='flex items-center'>
+                  <div className='w-8 h-8 relative translate-x-[20px]'>
+                    <Image fill src={SuccesssSVG} alt="plans"/>
+                  </div>
+                  <div></div>
+              </div>
             </div>
           </div>
         </div>
