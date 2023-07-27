b74bd7f20de039d3dd994c1fec9a508c4bfe490e
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-07-26 22:12:21 -0700

    updated

---
+++ app/About/page.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+const page : FC = () => {
+  return (
+    <div className='w-full min-h-screen bg-white'>
+      "THIS IS ABOUT PAGE "
+    </div>
+  )
+}
+page.displayName="page"
+export default page
